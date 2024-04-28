c317fc62213cd14fd22f06c18068f7a2c6226e14
MaheenSheikh <maheensheikh865@gmail.com>
2024-04-28 18:29:31 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+// conditional statement
+if (answer.operator === "addition") {
+  console.log(answer.firstNumber + answer.secondnumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondnumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstNumber * answer.secondnumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstNumber / answer.secondnumber);
+} else {
+  console.log("please select valid operator ");
+}
